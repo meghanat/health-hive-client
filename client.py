3838e0e46ba6b6cfaaa2863b1a7ae279877dc556
meghs.t@gmail.com <meghanathiyyakat@Meghanas-MacBook-Air.local>
2017-03-15 21:58:16 +0530

    Get CSV File

--- client.py
+++ client.py
@@ -3,12 +3,14 @@ username=raw_input("Enter username of mysql db ")
 password=raw_input("Enter password of mysql db ")
 db=raw_input("Enter the name of the database ")
 
-print username,password,db
+# print username,password,db
 
 db = MySQLdb.connect("localhost",username,password,db )
+# db = MySQLdb.connect("localhost","root","","somedb" )
 cursor = db.cursor()
 
 tablename=raw_input("Enter the name of the table to export ")
+# tablename="activities_ayur_consultant"
 query="describe "+tablename+";";
 cursor.execute(query)
 columns=cursor.fetchall()
@@ -18,10 +20,21 @@ for column in columns:
 
 
 export_columns=raw_input("Enter the column you wish to export (seperated by columns)\n")
+# export_columns="patient_id,prakruthi"
 
 print export_columns
 
 query="select "+export_columns+" from "+tablename+";"
 cursor.execute(query)
 rows=cursor.fetchall()
-print rows
+csv_header_row='"'+export_columns.replace(",",'","')+'"\n'
+filename=tablename+".csv"
+csv_file=open(filename,"w")
+csv_file.write(csv_header_row)
+
+
+for i in rows:
+	print i
+	row=",".join(map(str,i))
+	row+="\n"
+	csv_file.write( row)
